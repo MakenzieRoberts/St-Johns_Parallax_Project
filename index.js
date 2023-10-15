c36bd3d0a2fb3e507c0eb6c7fad5eae469b19db1
MakenzieRoberts <makenzie.roberts@keyin.com>
2023-10-15 15:52:45 -0230

    Fix nightbox covering scroll
    Put nightbox div inside the .parallax, changed position to sticky and changed blend mode to darken. The effect looks exactly the same but now doesn't interfere with scrolling.

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 let layers = document.getElementsByClassName("parallax__layer");
+const translateTarget = document.querySelectorAll(".scroll");
 console.log(layers);
 
 for (let layer of layers) {
 	console.log(layer);
 }
-// $parallax__layers: 6;
-
-// @for $i from 0 through $parallax__layers {
-//   $x: ($parallax__layers - $i) / 2;
-//   .parallax__layer__#{$i}{
-//      transform: translateZ(-100 * $x#{px}) scale($x + 1);
-//   }
-// }
 
 let numLayers = 6;
 
@@ -26,3 +19,39 @@ for (let i = 0; i < numLayers; i++) {
 	console.log("layers[i] style:", layers[i].style);
 }
 // style.transform = ` translateY(${x * 0.25}px ) `;
+
+// var objTable = document.getElementById("parallax-container");
+
+window.addEventListener(
+	"scroll",
+	function (e) {
+		console.log("scrolling");
+		translateOnScroll(e);
+		// scaleOnScroll(e);
+		// zOnScroll(e);
+	},
+	false
+);
+
+function translateOnScroll(e) {
+	console.log("translateOnScroll parallaxFinetune.js SCROLL EVENT...");
+	console.log(e);
+	var i = 0,
+		length = translateTarget.length;
+	for (i; i < length; i++) {
+		applyTransition(translateTarget[i]);
+		// Dividing by 1000 gives more control over speed
+		var pos = (window.pageYOffset * translateTarget[i].dataset.rate) / 1000;
+		if (translateTarget[i].dataset.direction === "vertical") {
+			translateTarget[i].style.transform = `translate3d(0px, ${pos}px, 0px)`;
+		}
+		if (translateTarget[i].dataset.direction === "horizontal") {
+			// Dividing by 1000 gives more control over speed
+			var posX = (window.pageYOffset * translateTarget[i].dataset.ratex) / 1000;
+			var posY = (window.pageYOffset * translateTarget[i].dataset.ratey) / 1000;
+			translateTarget[i].style.transform =
+				"translate3d(" + posX + "px, " + posY + `px, 0px)`;
+		}
+		console.log(translateTarget[i].dataset);
+	}
+}
